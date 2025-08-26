6c1aa2e9e19eefc9df5122ec7d34c2032e538e61
GPadaka19 <gustipadaka19@gmail.com>
2025-08-26 15:58:33 +0700

    feat: add Firebase configuration files and initial HTML setup for hosting

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-// Import the functions you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"; 
 import { getFunctions } from "firebase/functions";
 
-// ✅ Masukkan config dari Firebase Console
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -15,10 +13,8 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services yang dipakai
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
